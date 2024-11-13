4ac111986888f6d35bf36c4aaba02105e5c25a91
AmandhaPanagoda <amandhaisiri@gmail.com>
2024-11-13 21:10:19 +0530

    initial commit - seperated the cli project

---
+++ src/models/VIPCustomer.java
@@ -0,0 +1,17 @@
+public class VIPCustomer extends Customer {
+    public VIPCustomer(String name, TicketPool ticketPool) {
+        super(name, ticketPool);
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                getTicketPool().removeVIPTicket(getName());
+                Thread.sleep(getCustomerRetrievalRate());
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
